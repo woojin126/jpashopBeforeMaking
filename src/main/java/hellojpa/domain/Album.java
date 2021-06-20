a363c487df4e63e865e618a44066402ffd503362
woojin126 <woojin126@naver.com>
2021-06-20 14:30:19 +0900

    Album entity

---
+++ src/main/java/hellojpa/domain/Album.java
@@ -0,0 +1,17 @@
+package hellojpa.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.websocket.server.ServerEndpoint;
+
+@Entity
+@Getter
+@Setter
+public class Album extends Item{
+
+    private String artist;
+    private String etc;
+
+}
