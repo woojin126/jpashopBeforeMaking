490b2d1b398485879afe5a6e7b6505350834dd79
woojin126 <woojin126@naver.com>
2021-06-20 14:30:58 +0900

    OrderItem entity ORDER ITEM 중간 엔티티 1대 다 다대 1 로풀어냄

---
+++ src/main/java/hellojpa/domain/OrderItem.java
@@ -0,0 +1,30 @@
+package hellojpa.domain;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ORDER_ITEM_ID")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "ORDER_ID")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "ITEM_ID")
+    private Item item;
+
+    private int orderPrice;
+
+    private int count;
+}
