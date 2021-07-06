30d2ad4c9ee9fc45da45f02eb8adf90d3925adfa
woojin126 <woojin126@naver.com>
2021-07-06 22:18:24 +0900

    main

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,46 +19,9 @@ public class JpaMain {
         tx.begin();
         try{
 
-            Member member = new Member();
-            member.setName("김우진");
-            member.setCity("미국");
-            member.setStreet("신나무실");
-            member.setZipcodes("집코드");
-            em.persist(member);
-
-            Delivery delivery = new Delivery();
-            delivery.setCity(member.getCity());
-            delivery.setStreet(member.getStreet());
-            delivery.setZipcode(member.getZipcodes());
-
-            Book book = new Book();
-            book.setAuthor("인직");
-            book.setName("그리움");
-            em.persist(book);
-
-            OrderItem orderItem = new OrderItem();
-            orderItem.setItem(book);
 
 
 
-            Order order = new Order();
-            order.setCreateBy("날짜");
-            order.setMember(member);
-            order.setDelivery(delivery);
-            order.addOrderItem(orderItem);
-
-            em.persist(order);
-
-            em.flush();
-            em.clear();
-
-           // Order order1 = em.find(Order.class, order.getId());
-
-            System.out.println("-----------------------------");
-            Order or = em.getReference(Order.class, order.getId());
-
-            System.out.println("refInfo==" + or.getCreateBy());
-            System.out.println("-----------------------------");
             tx.commit();
 
         }catch(Exception e){
